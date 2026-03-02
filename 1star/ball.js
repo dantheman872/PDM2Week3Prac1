11c7fe2cf1fc47bdbbb83e5202c3db1d20f8fbaf
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2026-03-02 14:19:37 +0000

    1

--- 1star/ball.js
+++ 1star/ball.js
@@ -1,18 +1,80 @@
-class ball {
+class Ball {
+    #x;
+    #y;
+    #w;
 
-    x;
-    y;
-    size;
 
-    constructor(x, y, size){
-
-        this.x = x
-        this.y = y
-        this.size = size
+    /**
+     * Creates a new ball.
+     * @param {number} x The ball's x coordinate
+     * @param {number} y The ball's y coordinate
+     * @param {number} w The diameter of the ball
+     */
+    constructor(x, y, w) {
+        this.#x = x;
+        this.#y = y;
+        this.#w = w;
     }
 
-    display(){
 
-        ellipse(this.x, this.y, this.size)
+    /**
+     * Gets the x coordinate.
+     * @returns {number}
+     */
+    getX() {
+        return this.#x;
+    }
+
+
+    /**
+     * Gets the y coordinate.
+     * @returns {number}
+     */
+    getY() {
+        return this.#y;
+    }
+
+
+    /**
+     * Draws the ball
+     */
+    show() {
+        circle(this.#x, this.#y, this.#w);
+    }
+
+
+    /**
+     * Moves the ball by the given amount
+     * @param {number} deltaX The amount to move on the x axis
+     * @param {number} deltaY The amount to move on the y axis
+     * @example
+     * const b = new Ball(100, 50, 200);
+     * b.moveBy(2, -5);
+     * 
+     * The ball will move 2 pixels to the right and 5 pixels up to 102, 45.
+     */
+    move(deltaX, deltaY) {
+        this.#x += deltaX;
+        this.#y += deltaY;
+    }
+
+
+    /**
+     * Jumps to the given coordinate
+     * @param {number} newX 
+     * @param {number} newY 
+     */
+    jump(newX, newY) {
+        this.#x = newX;
+        this.#y = newY;
+    }
+
+
+    /**
+     * Checks if the mouse is over the ball
+     * @returns {boolean}
+     */
+    isMouseOver() {
+        return dist(mouseX, mouseY, this.#x, this.#y) <= this.#w;
     }
 }
--- 1star/sketch.js
+++ 1star/sketch.js
@@ -1,11 +1,33 @@
-function setup(){
+let bouncingBalls = [];
+const COUNT = 10;
+const DIAMETER = 50;
 
-    createCanvas(400, 400)
-    const whiteBall = new ball(50, 50, 50)
+function setup() {
+    createCanvas(400, 400);
+
+    for (let i = 0; i < COUNT; i++) {
+        bouncingBalls.push(new Ball(random(DIAMETER / 2, width - DIAMETER / 2), random(DIAMETER / 2, height - DIAMETER / 2), DIAMETER));
+    }
 }
 
-function draw(){
 
-    background(220)
-    ball.display()    
+function draw() {
+    background(0);
+    for (const ball of bouncingBalls) {
+        ball.show();
+        if (ball.isMouseOver()) {
+            // This syntax is a ternary - a shorthand way to write a simple if else. 
+            const deltaX = mouseX - pmouseX > 0 ? DIAMETER / 2 : -DIAMETER / 2;
+            const deltaY = mouseY - pmouseY > 0 ? DIAMETER / 2 : -DIAMETER / 2;
+            ball.move(deltaX, deltaY);
+        }
+    }
+}
+
+function keyPressed() {
+    if (key === "r") {
+        for (const ball of bouncingBalls) {
+            ball.jump(random(DIAMETER / 2, width - DIAMETER / 2), random(DIAMETER / 2, height - DIAMETER / 2));
+        }
+    }
 }
